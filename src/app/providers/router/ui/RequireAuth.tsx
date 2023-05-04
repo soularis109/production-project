6ef1cebd262819b1f74858b530f365a2a5d1f3d5
kostya <soularis109@gmail.com>
2023-05-04 12:20:41 +0300

    add private route v6

---
+++ src/app/providers/router/ui/RequireAuth.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getUserAuthData } from 'entities/User';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+
+const RequireAuth = ({ children }: {children:JSX.Element}) => {
+    const auth = useSelector(getUserAuthData);
+    const location = useLocation();
+
+    if (!auth) {
+        return <Navigate to={RoutePath.main} state={{ from: location }} replace />;
+    }
+    return children;
+};
+
+export default RequireAuth;
