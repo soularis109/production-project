0277045615d5264e4d47f58df054bbee906b81e0
kostya <soularis109@gmail.com>
2023-05-16 16:23:57 +0300

    add Auth netlify

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,9 +1,15 @@
 import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+import { config } from '@storybook/addon-actions';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
